ce47f601a30324d909ddf8cc032f28d3f445f0d1
Edgardo <ramirezedgardo92@gmail.com>
2023-04-19 04:30:10 -0600

    create user upload

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 /* paginas */
 import { RegisterComponent } from './register/register.component';
@@ -12,6 +14,13 @@ import { AuthRoutingModule } from './auth.routing';
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   exports: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
 })
 export class AuthModule {}
